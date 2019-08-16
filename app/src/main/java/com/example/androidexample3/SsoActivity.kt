2e9be9eac639a4c0a29fde4b0a99c06bde11a457
Jia Tse <jia.tse@zocdoc.com>
2019-08-16 12:09:00 -0400

    initial checkin

---
+++ app/src/main/java/com/example/androidexample3/SsoActivity.kt
@@ -0,0 +1,14 @@
+package com.example.androidexample3
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SsoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sso)
+    }
+}
