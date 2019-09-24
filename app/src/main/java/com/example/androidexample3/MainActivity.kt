f0800acba66eef0804186f1ff0e6ae8818e56417
Jia Tse <jam0cam@yahoo.com>
2019-09-23 22:49:33 -0400

    added some styling to a chip button for search facets

--- app/src/main/java/com/example/androidexample3/MainActivity.kt
+++ app/src/main/java/com/example/androidexample3/MainActivity.kt
@@ -12,5 +12,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         main_sso.setOnClickListener { startActivity(Intent(this, SsoActivity::class.java))  }
+
+
+
+        insuranceChip = View.inflate(this, R.layout.facets_chip, null) as Chip
+        filterChip = View.inflate(this, R.layout.facets_chip, null) as Chip
+
+        sidePadding = resources.getDimensionPixelSize(R.dimen.app_global_side_padding)
+        sideHalfPadding = resources.getDimensionPixelSize(R.dimen.app_global_side_half_half_padding)
+
+        linear_layout.addView(insuranceChip)
+        linear_layout.addView(filterChip)
+
+        insuranceChip.text = "Cigna: Open Access Plan"
+        filterChip.text = "more filters(2)"
+
+        insuranceChip.layoutParams = getChipLayoutParams(sidePadding!!)
+        filterChip.layoutParams = getChipLayoutParams(sideHalfPadding!!)
+    }
+
+
+    fun getChipLayoutParams(leftMar: Int) = LayoutParams(LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT).apply {
+        setMargins(leftMar, 0, 0, 0)
     }
 }
